3d3c4733ea9377d843aefedc1ad0809e9a4772da
oleksandr_vienik <alexandrVenik@gmail.com>
2023-11-07 13:04:17 +0200

    add button styles

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <script setup>
 import IButton from './components/IButton/IButton.vue'
-
-const classes = {
-  'bg-black': true,
-  'btn-red': true
-}
 </script>
 
 <template>
-  <span class="btn m-5 block bg-slate-400">Hello span</span>
-  <div>Hello Vue <IButton :class="classes"></IButton></div>
+  <IButton></IButton>
 </template>
-
-<style scoped>
-.btn {
-  color: green;
-}
-
-.btn-red {
-  color: red;
-}
-
-.bg-black {
-  background: #000;
-}
-</style>
--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,9 +1,5 @@
 <template>
-  <button class="btn">Click me</button>
+  <button class="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider">
+    Почати роботу
+  </button>
 </template>
-
-<style scoped>
-.btn {
-  color: red;
-}
-</style>
