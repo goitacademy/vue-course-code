7d24f46671a8e1272ca6a97601da087f09086036
oleksandr_vienik <alexandrVenik@gmail.com>
2023-11-13 02:26:35 +0200

    add v-if, v-show

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <script setup>
-import IButton from './components/IButton/IButton.vue'
+import HomepageView from './views/HomepageView.vue'
+
+const isDesktop = window.innerWidth > 1024
+const isTablet = window.innerWidth <= 1024 && window.innerWidth > 748
 </script>
 
 <template>
-  <main class="flex h-screen">
-    <section class="flex-1 flex justify-center items-center px-5 bg-primary">
-      <div class="text-white text-center">
-        <img class="inline mb-6" src="./assets/img/map-pin.svg" alt="" />
-        <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
-        <p class="leading-6 mb-11">
-          Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також
-          ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
-        </p>
-        <IButton></IButton>
-      </div>
-    </section>
-    <section class="flex-1">
-      <img class="h-full object-cover" src="./assets/img/static-map.png" alt="" />
-    </section>
-  </main>
+  <template v-if="isDesktop">
+    <HomepageView />
+  </template>
+  <template v-else-if="isTablet">This is tablet</template>
+  <template v-else>
+    <div v-show="!isDesktop && !isTablet">this is mobile</div>
+  </template>
+  <div v-show="!isDesktop && !isTablet">this is mobile</div>
 </template>
---
+++ src/views/HomepageView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import IButton from '../components/IButton/IButton.vue'
+</script>
+
+<template>
+  <main class="flex h-screen">
+    <section class="flex-1 flex justify-center items-center px-5 bg-primary">
+      <div class="text-white text-center">
+        <img class="inline mb-6" src="../assets/img/map-pin.svg" alt="" />
+        <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
+        <p class="leading-6 mb-11">
+          Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також
+          ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
+        </p>
+        <IButton></IButton>
+      </div>
+    </section>
+    <section class="flex-1">
+      <img class="h-full w-full object-cover" src="../assets/img/static-map.png" alt="" />
+    </section>
+  </main>
+</template>
