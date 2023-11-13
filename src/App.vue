0e8972a9104e48f33c9775e167d44426edf235d5
oleksandr_vienik <alexandrVenik@gmail.com>
2023-11-13 16:02:06 +0200

    add base Favorite place styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import HomepageView from './views/HomepageView.vue'
+import FavoritePlace from './components/FavoritePlace/FavoritePlace.vue'
 </script>
 
 <template>
-  <HomepageView />
+  <!-- <HomepageView /> -->
+  <div class="bg-white h-screen w-[400px]">
+    <FavoritePlace />
+  </div>
 </template>
---
+++ src/components/FavoritePlace/FavoritePlace.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <section class="text-[#939393] mb-6 last:mb-0">
+    <div class="flex gap-4">
+      <img class="w-[76px] h-[76px] shrink-0" src="" alt="" />
+      <div>
+        <div class="flex justify-between items-center mb-2">
+          <h2 class="font-bold text-sm text-[#2C2C2C]">Палац Спорту</h2>
+          <div class="flex gap-2">
+            <button>edit</button>
+            <button>delete</button>
+          </div>
+        </div>
+        <p class="text-xs line-clamp-3">
+          Ки́ївський пала́ц спо́рту (КПС) — найбільша крита спортивно-видовищна споруда України,
+          розташована у центральній частині міста Київ, біля підніжжя Черепанової гори.
+        </p>
+      </div>
+    </div>
+
+    <div class="h-[1px] w-full bg-[#ececec] mt-4"></div>
+  </section>
+</template>
