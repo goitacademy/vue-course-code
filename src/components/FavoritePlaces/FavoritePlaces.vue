45967350ebbcb2a23ff3ca8a776e462c893f9ff3
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-08 01:42:38 +0200

    reactive

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,10 +1,12 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 const counter = ref(0) // { value: 0 }
 const user = ref({ name: 'Tom', age: 14 })
+const superUser = reactive({ name: 'Super user', age: 1000 })
+const { name, age } = superUser
 
 const increment = () => {
   counter.value += 1
@@ -13,11 +15,16 @@ const increment = () => {
 const changeUserName = () => {
   user.value.name = 'Harry Potter'
 }
+
+const changeSuperUserName = () => {
+  superUser.name = 'New super user'
+}
 </script>
 
 <template>
   <div class="px-6">
     <div class="text-gray">User name: {{ user.name }}</div>
+    <div class="text-gray">Super User name: {{ name }}</div>
     <div class="text-gray mb-4">Додані маркери ({{ counter }})</div>
     <slot name="label"></slot>
     <slot name="list">
@@ -26,6 +33,7 @@ const changeUserName = () => {
 
     <slot></slot>
     <IButton class="w-full" @click="changeUserName">Change name</IButton>
+    <IButton class="w-full mt-2" @click="changeSuperUserName">Change super name</IButton>
     <IButton class="w-full mt-10" @click="increment">Додати маркер</IButton>
   </div>
 </template>
