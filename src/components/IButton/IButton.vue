ce04172437ab11b4fd2ddb9f6334cb0dbf9ad84f
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-08 00:06:33 +0200

    add list of favorite places

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,6 +1,5 @@
 <template>
   <button class="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider">
-    <div v-if="$slots.default">Hello default slot</div>
-    <slot>Hello button</slot>
+    <slot></slot>
   </button>
 </template>
