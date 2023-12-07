be172c9fffc54622c79ddae46e3f316d7ad939f7
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-08 01:23:08 +0200

    add ref

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,6 @@ import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 <template>
   <!-- <HomepageView /> -->
   <div class="bg-white h-screen w-[400px]">
-    <FavoritePlaces>
-      <div>THis is super slot</div>
-      <template v-slot:label>This is label</template>
-      <template #list>This is list</template>
-    </FavoritePlaces>
+    <FavoritePlaces />
   </div>
 </template>
--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,17 +1,31 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
+import { ref } from 'vue'
+
+const counter = ref(0) // { value: 0 }
+const user = ref({ name: 'Tom', age: 14 })
+
+const increment = () => {
+  counter.value += 1
+}
+
+const changeUserName = () => {
+  user.value.name = 'Harry Potter'
+}
 </script>
 
 <template>
   <div class="px-6">
-    <div class="text-gray mb-4">Додані маркери</div>
+    <div class="text-gray">User name: {{ user.name }}</div>
+    <div class="text-gray mb-4">Додані маркери ({{ counter }})</div>
     <slot name="label"></slot>
     <slot name="list">
       <FavoritePlace v-for="n in 4" :key="n" />
     </slot>
 
     <slot></slot>
-    <IButton class="w-full mt-10">Додати маркер</IButton>
+    <IButton class="w-full" @click="changeUserName">Change name</IButton>
+    <IButton class="w-full mt-10" @click="increment">Додати маркер</IButton>
   </div>
 </template>
