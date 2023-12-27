edf8019474ae4e811f6123ce45ac44f85e632d36
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-28 01:15:56 +0200

    add input starter

---
+++ src/components/IInput/IInput.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="w-full text-[#2C2C2C]">
+    <label class="block">
+      <span class="block text-xs px-3 mb-2">Some label</span>
+      <input
+        class="w-full text-sm rounded-[4px] border-[#eaeaea] border-[1px] py-2 px-3 focus:outline-primary"
+        type="text"
+      />
+    </label>
+  </div>
+</template>
