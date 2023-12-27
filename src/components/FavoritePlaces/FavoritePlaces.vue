1472c65c12597f1153381c19ed114a7fcd97deec
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-28 01:24:22 +0200

    add focus example

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,7 +1,8 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
+import IInput from '../IInput/IInput.vue'
 
 const buttonVariant = ref('gradient')
 
@@ -11,8 +12,11 @@ const changeButtonVariant = () => {
 </script>
 
 <template>
-  <div class="px-6">
+  <div class="px-6" @click="() => console.log('click')">
     <div class="text-gray mb-4">Додані маркери</div>
+    <div class="py-5">
+      <IInput label="Some label" />
+    </div>
     <slot name="label"></slot>
     <slot name="list">
       <FavoritePlace v-for="n in 4" :key="n" />
--- src/components/IInput/IInput.vue
+++ src/components/IInput/IInput.vue
@@ -1,10 +1,15 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  label: String
+})
+</script>
 
 <template>
   <div class="w-full text-[#2C2C2C]">
     <label class="block">
-      <span class="block text-xs px-3 mb-2">Some label</span>
+      <span class="block text-xs px-3 mb-2">{{ props.label }}</span>
       <input
+        @focus="() => console.log('focused')"
         class="w-full text-sm rounded-[4px] border-[#eaeaea] border-[1px] py-2 px-3 focus:outline-primary"
         type="text"
       />
