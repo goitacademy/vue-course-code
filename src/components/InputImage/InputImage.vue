82b211ce5a2d27ca5606fbb34db7d2ff3e1f2748
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 00:20:28 +0200

    finished file upload file size error

--- src/components/InputImage/InputImage.vue
+++ src/components/InputImage/InputImage.vue
@@ -1,14 +1,36 @@
 <script setup>
+import { ref } from 'vue'
 import UploadIcon from './UploadIcon.vue'
+
+const emit = defineEmits(['uploaded'])
+const errorMessage = ref('')
+
+const handleUploadImg = (event) => {
+  const file = event.target.files[0]
+
+  if (file.size > 3 * 1024 * 1024) {
+    errorMessage.value = 'Завеликий файл'
+    return
+  }
+
+  const fileReader = new FileReader()
+
+  fileReader.readAsDataURL(file)
+
+  fileReader.onload = () => {
+    errorMessage.value = ''
+    emit('uploaded', fileReader.result)
+  }
+}
 </script>
 
 <template>
   <label class="cursor-pointer hover:text-primary">
-    <input type="file" accept="image/*" class="hidden" />
+    <input type="file" accept="image/*" class="hidden" @change="handleUploadImg" />
     <span class="flex gap-1 items-center">
       <UploadIcon />
       <span class="underline text-xs">Натисніть тут, щоб додати фото</span>
     </span>
   </label>
-  <span class="text-red-500"></span>
+  <div v-if="errorMessage" class="text-red-500">{{ errorMessage }}</div>
 </template>
