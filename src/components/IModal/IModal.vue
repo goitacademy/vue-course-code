3f059f6402acc25f731316ce5c96546f1e162317
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 00:51:03 +0200

    add create place modal

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -1,10 +1,8 @@
 <script setup>
-import { onMounted, onUnmounted, ref, Teleport } from 'vue'
+import { onMounted, onUnmounted, Teleport } from 'vue'
 import CrossIcon from '../icons/CrossIcon.vue'
-import InputImage from '../InputImage/InputImage.vue'
 
 const emit = defineEmits(['close'])
-const imageUrlBase64 = ref('')
 
 onMounted(() => {
   document.body.style.overflow = 'hidden'
@@ -25,8 +23,6 @@ onUnmounted(() => {
         <button>
           <CrossIcon class="absolute right-3 top-3 w-6 h-6" @click="emit('close')" />
         </button>
-        <img v-if="imageUrlBase64" :src="imageUrlBase64" alt="" />
-        <InputImage @uploaded="(base64) => (imageUrlBase64 = base64)" />
         <slot></slot>
       </div>
     </div>
