b0a9e3cabb89cd67a8bf57caa8ae279c0e675edf
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-28 17:46:43 +0200

    add base modal

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -1,13 +1,25 @@
 <script setup>
+import { onMounted, onUnmounted } from 'vue'
 import CrossIcon from '../icons/CrossIcon.vue'
+
+const emit = defineEmits(['close'])
+
+onMounted(() => {
+  document.body.style.overflow = 'hidden'
+})
+
+onUnmounted(() => {
+  document.body.style.overflow = 'initial'
+})
 </script>
 
 <template>
   <div class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]">
     <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
       <button>
-        <CrossIcon class="absolute right-3 top-3 w-6 h-6" />
+        <CrossIcon class="absolute right-3 top-3 w-6 h-6" @click="emit('close')" />
       </button>
+      <slot></slot>
     </div>
   </div>
 </template>
