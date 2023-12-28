a525c9db67186e201b98673d349f128789e7565d
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-28 04:33:37 +0200

    add emits to submit form

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -1,13 +1,18 @@
 <script setup>
 import IButton from '../../IButton/IButton.vue'
 import IInput from '../../IInput/IInput.vue'
+
+const emit = defineEmits(['submit'])
 </script>
 
 <template>
-  <form class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
+  <form
+    @submit.prevent="emit('submit', { name: 'NAme' })"
+    class="max-w-[500px] w-full bg-white p-10 rounded-2xl"
+  >
     <IInput class="mb-4" label="Повне ім’я" />
     <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" />
-    <IInput label="Пароль" type="password" @input="(e) => console.log(e.target.value)" />
-    <IButton class="mt-10 w-full" variant="gradient">Створити аккаунт</IButton>
+    <IInput label="Пароль" type="password" />
+    <IButton class="mt-10 w-full" variant="gradient" type="submit">Створити аккаунт</IButton>
   </form>
 </template>
