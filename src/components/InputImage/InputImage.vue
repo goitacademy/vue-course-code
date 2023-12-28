19121fdad8a23ed9f06d3b5447a9d8cb096af8ec
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-28 23:43:18 +0200

    add starter for file input

---
+++ src/components/InputImage/InputImage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import UploadIcon from './UploadIcon.vue'
+</script>
+
+<template>
+  <label class="cursor-pointer hover:text-primary">
+    <input type="file" accept="image/*" class="hidden" />
+    <span class="flex gap-1 items-center">
+      <UploadIcon />
+      <span class="underline text-xs">Натисніть тут, щоб додати фото</span>
+    </span>
+  </label>
+  <span class="text-red-500"></span>
+</template>
