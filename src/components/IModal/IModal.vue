5170751d2b780ac65d00e602d3b67c771bf592f8
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-28 22:08:02 +0200

    add teleport to base modal

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, Teleport } from 'vue'
 import CrossIcon from '../icons/CrossIcon.vue'
 
 const emit = defineEmits(['close'])
@@ -14,12 +14,17 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]">
-    <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
-      <button>
-        <CrossIcon class="absolute right-3 top-3 w-6 h-6" @click="emit('close')" />
-      </button>
-      <slot></slot>
+  <Teleport to="body">
+    <div
+      class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]"
+      @click.self="emit('close')"
+    >
+      <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
+        <button>
+          <CrossIcon class="absolute right-3 top-3 w-6 h-6" @click="emit('close')" />
+        </button>
+        <slot></slot>
+      </div>
     </div>
-  </div>
+  </Teleport>
 </template>
