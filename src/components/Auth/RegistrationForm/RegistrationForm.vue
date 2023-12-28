c8b1465534a9b2a519fe02453c5b50be8da53ed4
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-28 02:39:09 +0200

    add new props to input

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -5,7 +5,7 @@ import IInput from '../../IInput/IInput.vue'
 <template>
   <form class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
     <IInput class="mb-4" label="Повне ім’я" />
-    <IInput class="mb-4" label="Електронна пошта" />
-    <IInput label="Пароль" />
+    <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" />
+    <IInput label="Пароль" type="password" />
   </form>
 </template>
