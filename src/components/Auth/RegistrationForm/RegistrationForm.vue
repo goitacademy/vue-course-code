466557d6237036136d18dff5ff06ee686cb479c0
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-28 04:15:16 +0200

    add submit button

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -1,4 +1,5 @@
 <script setup>
+import IButton from '../../IButton/IButton.vue'
 import IInput from '../../IInput/IInput.vue'
 </script>
 
@@ -6,6 +7,7 @@ import IInput from '../../IInput/IInput.vue'
   <form class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
     <IInput class="mb-4" label="Повне ім’я" />
     <IInput class="mb-4" label="Електронна пошта" placeholder="email@gmail.com" />
-    <IInput label="Пароль" type="password" disabled />
+    <IInput label="Пароль" type="password" @input="(e) => console.log(e.target.value)" />
+    <IButton class="mt-10 w-full" variant="gradient">Створити аккаунт</IButton>
   </form>
 </template>
