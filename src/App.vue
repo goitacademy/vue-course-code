e0e3f5a86e5a2c38cf99448b962a641e0137ed27
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 03:03:56 +0200

    map starting template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <script setup>
-import { ref } from 'vue'
-import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
-import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
-import CreateNewPlaceModal from './components/CreateNewPlaceModal/CreateNewPlaceModal.vue'
-
-const isOpen = ref(true)
-const closeModal = () => {
-  isOpen.value = false
-}
-const openModal = () => {
-  isOpen.value = true
-}
-
-// import HomepageView from './views/HomepageView.vue'
-// import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 </script>
 
 <template>
-  <!-- <HomepageView /> -->
-  <!-- <div class="bg-white h-screen w-[400px]">
-    <FavoritePlaces />
-  </div> -->
-  <button @click="openModal">Click me</button>
-  <RegistrationForm @submit="console.log" />
-  <LoginForm @submit="console.log" />
-  <CreateNewPlaceModal :is-open="isOpen" @close="closeModal" @submit="console.log" />
+  <main class="flex h-screen">
+    <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
+      <FavoritePlaces />
+    </div>
+    <div class="w-full h-full flex items-center justify-center text-6xl">map</div>
+  </main>
 </template>
