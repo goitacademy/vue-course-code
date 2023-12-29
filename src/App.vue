a8b3b6693ca4390edc5eb7ab863092b13aedf61b
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 04:50:33 +0200

    reworked favorite places

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,29 @@ import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 import { MapboxMap } from '@studiometa/vue-mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { mapSettings } from './map/settings'
+
+const favoritePlaces = [
+  {
+    id: 1,
+    title: 'New place 1',
+    description: 'SUper description 1',
+    img: '',
+    lngLat: []
+  },
+  {
+    id: 2,
+    title: 'New place 2',
+    description: 'SUper description 2',
+    img: '',
+    lngLat: []
+  }
+]
 </script>
 
 <template>
   <main class="flex h-screen">
     <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
-      <FavoritePlaces />
+      <FavoritePlaces :items="favoritePlaces" />
     </div>
     <div class="w-full h-full flex items-center justify-center text-6xl">
       <MapboxMap
--- src/components/FavoritePlace/FavoritePlace.vue
+++ src/components/FavoritePlace/FavoritePlace.vue
@@ -2,23 +2,38 @@
 import FavoritePlaceIconButton from './FavoritePlaceIconButton.vue'
 import DeleteIcon from './DeleteIcon.vue'
 import EditIcon from './EditIcon.vue'
+
+const props = defineProps({
+  title: {
+    required: true,
+    type: String
+  },
+  description: {
+    required: true,
+    type: String
+  },
+  img: String
+})
 </script>
 
 <template>
   <section class="text-gray mb-6 last:mb-0">
     <div class="flex gap-4">
-      <img class="w-[76px] h-[76px] shrink-0" src="" alt="" />
-      <div>
+      <img class="w-[76px] h-[76px] shrink-0" :src="props.img" alt="" />
+      <div class="w-full">
         <div class="flex justify-between items-center mb-2">
-          <h2 class="font-bold text-sm text-[#2C2C2C]">Палац Спорту</h2>
+          <h2 class="font-bold text-sm text-[#2C2C2C]">{{ props.title }}</h2>
           <div class="flex gap-2">
-            <FavoritePlaceIconButton><EditIcon /></FavoritePlaceIconButton>
-            <FavoritePlaceIconButton><DeleteIcon /></FavoritePlaceIconButton>
+            <FavoritePlaceIconButton>
+              <EditIcon />
+            </FavoritePlaceIconButton>
+            <FavoritePlaceIconButton>
+              <DeleteIcon />
+            </FavoritePlaceIconButton>
           </div>
         </div>
         <p class="text-xs line-clamp-3">
-          Ки́ївський пала́ц спо́рту (КПС) — найбільша крита спортивно-видовищна споруда України,
-          розташована у центральній частині міста Київ, біля підніжжя Черепанової гори.
+          {{ props.description }}
         </p>
       </div>
     </div>
