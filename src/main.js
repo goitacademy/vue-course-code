6723ef865964c388ebad71f8d600968bce85c132
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 07:44:58 +0200

    router setup

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { router } from './router'
 
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
