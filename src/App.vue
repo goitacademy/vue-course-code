34b0d69ca12d7a35ad7ef63ad5f5ba374979ee60
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 06:59:42 +0200

    create homepage view

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,7 @@
 <script setup>
-import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
-import { MapboxMap, MapboxMarker } from '@studiometa/vue-mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
-import { mapSettings } from './map/settings'
-import MarkerIcon from './components/icons/MarkerIcon.vue'
-import { ref } from 'vue'
-
-const favoritePlaces = [
-  {
-    id: 1,
-    title: 'New place 1',
-    description: 'SUper description 1',
-    img: '',
-    lngLat: [30.523333, 50.490001]
-  },
-  {
-    id: 2,
-    title: 'New place 2',
-    description: 'SUper description 2',
-    img: '',
-    lngLat: [30.523333, 50.450001]
-  }
-]
-
-const activeId = ref(null)
-const map = ref(null)
-
-const changeActiveId = (id) => {
-  activeId.value = id
-}
-
-const changePlace = (id) => {
-  const { lngLat } = favoritePlaces.find((place) => place.id === id)
-  changeActiveId(id)
-  map.value.flyTo({ center: lngLat })
-}
+import HomepageView from './views/HomepageView.vue'
 </script>
 
 <template>
-  <main class="flex h-screen">
-    <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
-      <FavoritePlaces :items="favoritePlaces" :active-id="activeId" @place-clicked="changePlace" />
-    </div>
-    <div class="w-full h-full flex items-center justify-center text-6xl">
-      <MapboxMap
-        class="w-full h-full"
-        :center="[30.523333, 50.450001]"
-        :zoom="10"
-        :access-token="mapSettings.apiToken"
-        :map-style="mapSettings.style"
-        @mb-created="(mapInstance) => (map = mapInstance)"
-      >
-        <MapboxMarker v-for="place in favoritePlaces" :key="place.id" :lngLat="place.lngLat">
-          <button @click="changeActiveId(place.id)">
-            <MarkerIcon class="h-8 w-8" />
-          </button>
-        </MapboxMarker>
-      </MapboxMap>
-    </div>
-  </main>
+  <HomepageView />
 </template>
