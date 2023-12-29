2385a6eafedf1c992c1e5c70da0554b346b705fd
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 07:21:52 +0200

    add rest views

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import HomepageView from './views/HomepageView.vue'
+import GreetingView from './views/GreetingView.vue'
+import AuthView from './views/AuthView.vue'
 </script>
 
 <template>
-  <HomepageView />
+  <AuthView />
+  <!-- <GreetingView /> -->
+  <!-- <HomepageView /> -->
 </template>
