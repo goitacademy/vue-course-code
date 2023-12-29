1423e4284967b3b88427ce7d02fa0beb6b8bf246
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 04:50:33 +0200

    reworked favorite places

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,6 +1,13 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
+
+const props = defineProps({
+  items: {
+    required: true,
+    type: Array
+  }
+})
 </script>
 
 <template>
@@ -9,7 +16,13 @@ import IButton from '../IButton/IButton.vue'
 
     <slot name="label"></slot>
     <slot name="list">
-      <FavoritePlace v-for="n in 4" :key="n" />
+      <FavoritePlace
+        v-for="place in props.items"
+        :key="place.id"
+        :title="place.title"
+        :description="place.description"
+        :img="place.img"
+      />
     </slot>
 
     <slot></slot>
