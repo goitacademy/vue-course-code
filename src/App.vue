a45205c0d76a8a0a1f59c58016a72d5e834c761e
oleksandr_vienik <alexandrVenik@gmail.com>
2023-12-29 04:29:13 +0200

    setup mapbox

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 import { MapboxMap } from '@studiometa/vue-mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { mapSettings } from './map/settings'
 </script>
 
 <template>
@@ -10,7 +11,13 @@ import 'mapbox-gl/dist/mapbox-gl.css'
       <FavoritePlaces />
     </div>
     <div class="w-full h-full flex items-center justify-center text-6xl">
-      <MapboxMap class="w-full h-full"></MapboxMap>
+      <MapboxMap
+        class="w-full h-full"
+        :center="[30.523333, 50.450001]"
+        :zoom="10"
+        :access-token="mapSettings.apiToken"
+        :map-style="mapSettings.style"
+      ></MapboxMap>
     </div>
   </main>
 </template>
