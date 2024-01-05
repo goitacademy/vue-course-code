4d628cc67f363676cf4b8d1b3d01b7d92b680480
oleksandr_vienik <alexandrVenik@gmail.com>
2024-01-05 20:07:27 +0200

    add loading status to button and form

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -2,19 +2,24 @@
 import RegistrationForm from '../components/Auth/RegistrationForm/RegistrationForm.vue'
 import { registerUser } from '../api/user'
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
 
 const router = useRouter()
+const isLoading = ref(false)
 
 const handleRegisterUser = async (userData) => {
+  isLoading.value = true
   try {
     await registerUser(userData)
     router.replace('/map')
   } catch (error) {
     console.error(error)
+  } finally {
+    isLoading.value = false
   }
 }
 </script>
 
 <template>
-  <RegistrationForm @submit="handleRegisterUser" />
+  <RegistrationForm @submit="handleRegisterUser" :is-loading="isLoading" />
 </template>
