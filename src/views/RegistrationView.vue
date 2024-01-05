592709aa46fc945daecab153edf96ea42819dba9
oleksandr_vienik <alexandrVenik@gmail.com>
2024-01-05 19:57:49 +0200

    add registration endpoint

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,7 +1,20 @@
 <script setup>
 import RegistrationForm from '../components/Auth/RegistrationForm/RegistrationForm.vue'
+import { registerUser } from '../api/user'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleRegisterUser = async (userData) => {
+  try {
+    await registerUser(userData)
+    router.replace('/map')
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
 
 <template>
-  <RegistrationForm />
+  <RegistrationForm @submit="handleRegisterUser" />
 </template>
