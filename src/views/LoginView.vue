66160534ef75cda0e651c0f6206ca90318457fa6
oleksandr_vienik <alexandrVenik@gmail.com>
2024-01-09 01:57:02 +0200

    add login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,25 @@
 <script setup>
 import LoginForm from '../components/Auth/LoginForm/LoginForm.vue'
+import { login } from '../api/user'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const isLoading = ref(false)
+const router = useRouter()
+
+const handleLogin = async (userData) => {
+  isLoading.value = true
+  try {
+    await login(userData)
+    router.replace('/map')
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isLoading.value = false
+  }
+}
 </script>
 
 <template>
-  <LoginForm />
+  <LoginForm @submit="handleLogin" :is-loading="isLoading" />
 </template>
