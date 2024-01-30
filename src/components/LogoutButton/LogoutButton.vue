40958e1d393ed7f36910a73c6e8f2d3f1ad133d8
oleksandr_vienik <alexandrVenik@gmail.com>
2024-01-30 21:26:48 +0200

    add logout

--- src/components/LogoutButton/LogoutButton.vue
+++ src/components/LogoutButton/LogoutButton.vue
@@ -1,7 +1,23 @@
 <script setup>
+import { logout } from '../../api/user'
 import LogoutIcon from './LogoutIcon.vue'
+import { useMutation } from '../../composables/useMutation'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const { mutation: logoutUser, isLoading } = useMutation({
+  mutationFn: () => logout(),
+  onSuccess: () => {
+    router.replace('/auth/login')
+  }
+})
 </script>
 
 <template>
-  <button class="flex gap-2 items-center px-6 text-black">Вихід <LogoutIcon /></button>
+  <button class="flex gap-2 items-center px-6 text-black" @click="logoutUser">
+    <span v-if="isLoading">Loading...</span>
+    <span v-else>Вихід</span>
+    <LogoutIcon />
+  </button>
 </template>
