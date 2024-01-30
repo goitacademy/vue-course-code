e14d851cb31ae2442c9268e09baab3ce0262482f
oleksandr_vienik <alexandrVenik@gmail.com>
2024-01-30 20:57:44 +0200

    add logout and userInfo component

---
+++ src/components/UserInfo/UserIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    fill="currentColor"
+    height="24px"
+    width="24px"
+    version="1.1"
+    id="Layer_1"
+    viewBox="0 0 512 512"
+    xml:space="preserve"
+  >
+    <circle cx="256" cy="114.526" r="114.526" />
+    <path
+      d="M256,256c-111.619,0-202.105,90.487-202.105,202.105c0,29.765,24.13,53.895,53.895,53.895h296.421    c29.765,0,53.895-24.13,53.895-53.895C458.105,346.487,367.619,256,256,256z"
+    />
+  </svg>
+</template>
---
+++ src/components/UserInfo/UserInfo.vue
@@ -0,0 +1,14 @@
+<script setup>
+import UserIcon from './UserIcon.vue'
+</script>
+
+<template>
+  <div
+    class="flex sticky top-0 items-center text-black gap-3 bg-[#ffe6dc] border-solid border-b-2 border-primary rounded-bl-2xl rounded-br-2xl px-6 py-4 mb-10"
+  >
+    <div class="w-10 h-10 flex items-center justify-center rounded-full color-primary bg-primary">
+      <UserIcon class="text-white" />
+    </div>
+    <span>User name</span>
+  </div>
+</template>
