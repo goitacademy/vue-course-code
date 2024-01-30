b3f6ba275bae4440531a0a251758ee8f435a2a81
oleksandr_vienik <alexandrVenik@gmail.com>
2024-01-30 18:52:44 +0200

    add confirmation modal

---
+++ src/components/ConfirmationModal/ConfirmationModal.vue
@@ -0,0 +1,29 @@
+<script setup>
+import IButton from '../IButton/IButton.vue'
+import IModal from '../IModal/IModal.vue'
+
+defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  isOpen: {
+    type: Boolean,
+    default: false
+  },
+  isLoading: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <IModal>
+    <div class="mb-4 text-lg">{{ title }}</div>
+    <div class="flex gap-3 justify-center">
+      <IButton>Відхилити</IButton>
+      <IButton variant="gradient">Підтвердити </IButton>
+    </div>
+  </IModal>
+</template>
